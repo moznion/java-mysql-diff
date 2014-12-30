c45278013b06a424b746de5d85e902ce7cdc90c7
moznion <moznion@gmail.com>
2014-12-30 22:09:56 +0900

    Show friendly message
    Fix #3

--- src/main/java/net/moznion/mysql/diff/App.java
+++ src/main/java/net/moznion/mysql/diff/App.java
@@ -6,31 +6,64 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.StringArrayOptionHandler;
 
 import net.moznion.mysql.diff.model.Table;
 
 public class App {
+  @Option(name = "-v", aliases = "--version", usage = "print version")
+  private boolean showVersion;
+
+  @Option(name = "-h", aliases = "--help", usage = "print usage message and exit")
+  private boolean showUsage;
+
+  @Argument(index = 0, metaVar = "arguments...", handler = StringArrayOptionHandler.class)
+  private String[] arguments;
+
   public static void main(String[] args) throws IOException, SQLException, InterruptedException {
-    // TODO use log4j
-    if (args.length <= 0) {
-      // TODO show usage
-      throw new IllegalArgumentException("Too few command line arguments");
+    App app = new App();
+
+    CmdLineParser parser = new CmdLineParser(app);
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      e.printStackTrace();
+      return;
     }
 
-    if (args[0].equals("-version")) {
+    if (app.showVersion) {
       System.out.println(App.class.getPackage().getImplementationVersion());
       return;
     }
 
-    if (args.length != 2) {
-      // TODO show usage
-      throw new IllegalArgumentException("Too few command line arguments");
+    if (app.showUsage) {
+      System.out.println(getUsageMessage());
+      return;
+    }
+
+    List<String> coreArgs = Arrays.asList(Optional.ofNullable(app.arguments).orElse(new String[0]));
+    int numOfArgs = coreArgs.size();
+    if (numOfArgs != 2) {
+      if (numOfArgs < 2) {
+        System.err.println("[ERROR] Too few command line arguments");
+      } else {
+        System.err.println("[ERROR] Too many command line arguments");
+      }
+      System.err.println();
+      System.err.println(getUsageMessage());
+      System.exit(1);
     }
 
     List<List<Table>> parsed = new ArrayList<>();
-    for (String arg : args) {
+    for (String arg : coreArgs) {
       String schema;
       SchemaDumper schemaDumper = new SchemaDumper(); // TODO should be more configurable
 
@@ -104,5 +137,24 @@ public class App {
     }
 
     return builder.build();
+  }
+
+  private static String getUsageMessage() {
+    return "[Usage]\n" +
+        "    java -jar <old_database> <new_database>\n" +
+        "[Examples]\n" +
+        "* Take diff between createtable1.sql and createtable2.sql " +
+        "(both of them are SQL file which are on your machine)\n" +
+        "    java -jar createtable1.sql createtable2.sql\n" +
+        "* Take diff between dbname1 and dbname2 " +
+        "(both of databases on the local MySQL)\n" +
+        "    java -jar dbname1 dbname2\n" +
+        "* Take diff between dbname1 and dbname2 " +
+        "(both of databases on remote MySQL)\n" +
+        "    java -jar '-uroot -hlocalhost dbname1' '-uroot -hlocalhost dbname2'" +
+        "\n" +
+        "[Options]\n" +
+        "    -h, --help:    Show usage\n" +
+        "    -v, --version: Show version";
   }
 }
